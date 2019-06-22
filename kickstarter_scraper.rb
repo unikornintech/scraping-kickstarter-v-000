26a5bb45f82b14c512fb3a439e93478b541460ac
Lauren Days <lmchambers0420@gmail.com>
2019-06-22 23:38:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,16 +2,13 @@
 require 'nokogiri'
 require 'pry'
 
-require_relative './kickstarter.html'
 
 def create_project_hash
+# projects: kickstarter.css("li.project.grid_4")
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-end
-binding.pry 
+  binding.pry
+end 
 
 create_project_hash
-
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text 
