70eb11f3c568cf38684ee59b6dc7bcf0505b5e77
Lauren Days <lmchambers0420@gmail.com>
2019-06-23 00:32:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-
+# location: project.css('ul.project-meta li a').text 
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
