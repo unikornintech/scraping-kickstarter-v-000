f9809ccfd026c3c8d177da79eda202a0f53107ab
Lauren Days <lmchambers0420@gmail.com>
2019-06-23 01:04:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,7 +22,9 @@ def create_project_hash
   
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link =>
+    }
     
 end 
 
