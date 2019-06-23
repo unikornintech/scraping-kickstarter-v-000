d206742d2c31d97973b4214ffd227b095830e2da
Lauren Days <lmchambers0420@gmail.com>
2019-06-23 00:26:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ require 'pry'
 def create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 
   html = File.read('fixtures/kickstarter.html')
   
